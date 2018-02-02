93be15a56d6ac1e1fa4a49f7142526f476730c77
Alex Windle <alexwindle85@gmail.com>
2018-02-02 14:36:29 -0500

    refactor multi-bar response groupings

--- chart.js
+++ chart.js
@@ -49,7 +49,8 @@ function makeChart (chartConfigObject, jsonData, lookup) {
         for (key in lku) {
             if (code === key) {
                 const name = lku[code].name;
-                xAxisCategoryNames.push(name);
+                if (xAxisCategoryNames.indexOf(name) < 0)
+                    xAxisCategoryNames.push(name);
             } 
         }
     });
@@ -137,15 +138,149 @@ function makeChart (chartConfigObject, jsonData, lookup) {
             break;
         case 2: 
             console.log("display multi-bar chart");
-            // makeChartSingleBar();
+            makeChartMultiBar();
             break; 
         default:
             console.log("error"); 
             break; 
     }
 
-    //chart
-    function makeChartSingleBar () {
+    // function makeChartSingleBar () {
+    //     var chart = d3.select(".chart") 
+    //     // .attr("height", "700") //refactor for compatibility with ie
+    //     .attr("viewBox", function () {
+    //         return "0 0 700 700";
+    //     })
+    //     .attr("preserveAspectRatio", "xMinYMin meet");
+
+    //     //create bar grouping
+    //     var bar = chart.selectAll("g"); 
+
+    //     // add the Y gridlines
+    //     chart.append("g")			
+    //         .attr("class", "grid")
+    //         .call(make_y_gridlines()
+    //             .tickSize(-width) //full graph width
+    //             .tickFormat("")
+    //         )
+    //         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");    
+
+    //     //bars
+    //     bar = bar.data(xAxisCategoryNames)
+    //         .enter()
+    //         .append("g") 
+    //         .attr("transform", function (d) { 
+    //             var bandwidth = x.bandwidth(); 
+    //             var spaceLeft = x.bandwidth() + x(d);
+    //             return "translate(" + spaceLeft + ", " + margin.top + ")";  
+    //         }); 
+    //     bar.append("rect")
+    //         .data(barDataValues)
+    //         .attr("y", function (d) { return y(d); }) //y coordinate
+    //         .attr("height", function (d) { return height - y(d); }) //height
+    //         .attr("width", function (d, i) { return x.bandwidth() / 3; })
+    //         .style("fill", barColors[0]) //hard-coded first color in array
+    //         .data(tooltipDisplay)
+    //         .on("mouseover", function (d, i) {
+    //             div.transition()
+    //                 .duration(200)
+    //                 .style("opacity", .9);
+    //             div.html(`
+    //                 <h3>${d.title}</h3> 
+    //                 <h3>${d.dv}</h3>
+    //                 CI (${d.lci} - ${d.hci})
+    //                 <br />WN = ${d.wn}
+    //             `)                  
+    //                 .style("left", (d3.event.pageX - 70) + "px")
+    //                 .style("top", (d3.event.pageY - 90) + "px");
+    //         })
+    //         .on("mouseout", function (d) {
+    //             div.transition()
+    //                 .duration(500)
+    //                 .style("opacity", 0);
+    //         }); 
+
+    //     //confidence indicator line
+    //     var line = bar.append("line")
+    //         .attr("class", "confidence_indicator")
+    //         .data(confidenceIndicators)
+    //         .attr("x1", function () { return x.bandwidth() / 6; })
+    //         .attr("y1", function (d) { return y(d.lci); }) 
+    //         .attr("x2", function () { return x.bandwidth() / 6; }) 
+    //         .attr("y2", function (d) { return y(d.hci); });
+    //     //confidence indicator linecaps
+    //     var linecapHalfWidth = 5; 
+    //     var linecap_top = bar.append("line")
+    //         .attr("class", "linecap_top")
+    //         .data(confidenceIndicators)
+    //         .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
+    //         .attr("y1", function (d) { return y(d.hci); })
+    //         .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
+    //         .attr("y2", function (d) { return y(d.hci); });
+    //     var linecap_bottom = bar.append("line")
+    //         .attr("class", "linecap_top")
+    //         .data(confidenceIndicators)
+    //         .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
+    //         .attr("y1", function (d) { return y(d.lci); })
+    //         .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
+    //         .attr("y2", function (d) { return y(d.lci); });
+
+    //     //axes labels
+    //     var yAxisMidpoint = (height + margin.top)/2 + margin.top;    
+    //     var paddingLeft = 14;         
+    //     var yAxisLabel = chart.append("text")
+    //         .attr("class", "label")
+    //         .attr("id", "y_axis_label")
+    //         .text(yAxisTitle)
+    //         .attr("transform", "translate(" + paddingLeft + ", " + yAxisMidpoint + ")rotate(-90)")                
+    //         .attr("text-anchor", "middle");         
+
+    //     //axes
+    //     var xAxis = chart.append("g")
+    //         .attr("class", "axis")             
+    //         .attr("transform", "translate(" + margin.left + ", " + spaceFromTop + ")")
+    //         .call(d3.axisBottom(x))
+    //         .selectAll("text")
+    //         .style("text-anchor", "end")
+    //         .attr("dx", "-.8em")
+    //         .attr("dy", ".15em")
+    //         .attr("transform", "rotate(-45)");
+    //     var yAxis = chart.append("g")
+    //         .attr("class", "axis")
+    //         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+    //         .call(d3.axisLeft(y))
+    //         .select(".domain").remove(); //remove y-axis line
+
+    //     //legend 
+    //     var legend = chart.append("g") //create & position legend area
+    //         .attr("class", "legend")
+    //         .attr("transform", "translate(" + halfTotalWidth + ", " + chartBottomBufferLegend + ")");
+    //     var legendEntry =  legend.selectAll("g") //groupings do not exist yet
+    //         .data(legendCategoryNames) //count data
+    //         .enter() //run methods once per data count
+    //         .append("g") //produces new groupings
+    //         .attr("height", legendColorKeyHeight); 
+    //     var colorKey = legendEntry.append("rect")
+    //         .attr("width", legendColorKeyWidth)
+    //         .attr("height", legendColorKeyHeight)
+    //         .attr("transform", function (d, i) {
+    //             let legendItemYPosition = legendItemHeight*i;
+    //             return "translate(0, " + legendItemYPosition + ")";
+    //         })
+    //         .style("fill", function (d, i) {
+    //             return barColors[i];
+    //         });
+    //     var label = legendEntry.append("text")
+    //         .text(function (d) { return d; }) 
+    //         //.attr("height", legendColorKeyHeight)
+    //         .attr("transform", function (d, i) {
+    //             let legendItemYPosition = legendItemHeight*i;
+    //             let paddingLeft = legendColorKeyWidth*2; 
+    //             return "translate(" + paddingLeft + ", " + legendItemYPosition + ")";
+    //     });
+    // }
+
+    function makeChartMultiBar () {
         var chart = d3.select(".chart") 
         // .attr("height", "700") //refactor for compatibility with ie
         .attr("viewBox", function () {
@@ -154,7 +289,7 @@ function makeChart (chartConfigObject, jsonData, lookup) {
         .attr("preserveAspectRatio", "xMinYMin meet");
 
         //create bar grouping
-        var bar = chart.selectAll("g"); 
+        var responseGrouping = chart.selectAll("g"); 
 
         // add the Y gridlines
         chart.append("g")			
@@ -165,118 +300,146 @@ function makeChart (chartConfigObject, jsonData, lookup) {
             )
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");    
 
-        //bars
-        bar = bar.data(xAxisCategoryNames)
+        // calculate # of response areas and # of bars within each
+        responseGrouping = responseGrouping.data(xAxisCategoryNames) // makes 1 grouping per response
             .enter()
-            .append("g") 
+            .append("g")
+            .attr("class", "response_grouping")
             .attr("transform", function (d) { 
                 var bandwidth = x.bandwidth(); 
                 var spaceLeft = x.bandwidth() + x(d);
                 return "translate(" + spaceLeft + ", " + margin.top + ")";  
             }); 
-        bar.append("rect")
-            .data(barDataValues)
-            .attr("y", function (d) { return y(d); }) //y coordinate
-            .attr("height", function (d) { return height - y(d); }) //height
-            .attr("width", function (d, i) { return x.bandwidth() / 3; })
-            .style("fill", barColors[0]) //hard-coded first color in array
-            .data(tooltipDisplay)
-            .on("mouseover", function (d, i) {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                div.html(`
-                    <h3>${d.title}</h3> 
-                    <h3>${d.dv}</h3>
-                    CI (${d.lci} - ${d.hci})
-                    <br />WN = ${d.wn}
-                `)                  
-                    .style("left", (d3.event.pageX - 70) + "px")
-                    .style("top", (d3.event.pageY - 90) + "px");
-            })
-            .on("mouseout", function (d) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            }); 
+        
+        // create new grouping level to contain multiple bars. how many bars per grouping? 
+
+        var barMultiDisplay = responseGrouping.selectAll("g"); //does this exist yet? 
+        
+         //scale
+        var xBandwidth = x.bandwidth(); 
+        console.log("xBandwidth: ", xBandwidth);
+
+        var xResponseBand = d3.scaleBand()
+            .domain(legendCategoryNames) 
+            .rangeRound([0, xBandwidth]) //total width of response band (188)
+            .padding(0.1);
+
+        barMultiDisplay = barMultiDisplay.data(legendCategoryNames) // count 2 data items to display
+            .enter()
+            .append("g")
+            .attr("class", "bar_multi_display")
+            .attr("transform", function (d) {
+                var bandwidth = xResponseBand.bandwidth(); 
+                var spaceLeft = xResponseBand.bandwidth() + xResponseBand(d); 
+                console.log("bar spacing: ", spaceLeft);
+                return "translate(" + spaceLeft + ", " + margin.top + ")"; 
+            }); // accurate? 
+
+        
+    
+        // bar.append("rect")
+        //     .data(barDataValues)
+        //     .attr("y", function (d) { return y(d); }) //y coordinate
+        //     .attr("height", function (d) { return height - y(d); }) //height
+        //     .attr("width", function (d, i) { return x.bandwidth() / 3; })
+        //     .style("fill", barColors[0]) //hard-coded first color in array
+        //     .data(tooltipDisplay)
+        //     .on("mouseover", function (d, i) {
+        //         div.transition()
+        //             .duration(200)
+        //             .style("opacity", .9);
+        //         div.html(`
+        //             <h3>${d.title}</h3> 
+        //             <h3>${d.dv}</h3>
+        //             CI (${d.lci} - ${d.hci})
+        //             <br />WN = ${d.wn}
+        //         `)                  
+        //             .style("left", (d3.event.pageX - 70) + "px")
+        //             .style("top", (d3.event.pageY - 90) + "px");
+        //     })
+        //     .on("mouseout", function (d) {
+        //         div.transition()
+        //             .duration(500)
+        //             .style("opacity", 0);
+        //     }); 
 
         //confidence indicator line
-        var line = bar.append("line")
-            .attr("class", "confidence_indicator")
-            .data(confidenceIndicators)
-            .attr("x1", function () { return x.bandwidth() / 6; })
-            .attr("y1", function (d) { return y(d.lci); }) 
-            .attr("x2", function () { return x.bandwidth() / 6; }) 
-            .attr("y2", function (d) { return y(d.hci); });
+        // var line = bar.append("line")
+        //     .attr("class", "confidence_indicator")
+        //     .data(confidenceIndicators)
+        //     .attr("x1", function () { return x.bandwidth() / 6; })
+        //     .attr("y1", function (d) { return y(d.lci); }) 
+        //     .attr("x2", function () { return x.bandwidth() / 6; }) 
+        //     .attr("y2", function (d) { return y(d.hci); });
         //confidence indicator linecaps
-        var linecapHalfWidth = 5; 
-        var linecap_top = bar.append("line")
-            .attr("class", "linecap_top")
-            .data(confidenceIndicators)
-            .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
-            .attr("y1", function (d) { return y(d.hci); })
-            .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
-            .attr("y2", function (d) { return y(d.hci); });
-        var linecap_bottom = bar.append("line")
-            .attr("class", "linecap_top")
-            .data(confidenceIndicators)
-            .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
-            .attr("y1", function (d) { return y(d.lci); })
-            .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
-            .attr("y2", function (d) { return y(d.lci); });
+        // var linecapHalfWidth = 5; 
+        // var linecap_top = bar.append("line")
+        //     .attr("class", "linecap_top")
+        //     .data(confidenceIndicators)
+        //     .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
+        //     .attr("y1", function (d) { return y(d.hci); })
+        //     .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
+        //     .attr("y2", function (d) { return y(d.hci); });
+        // var linecap_bottom = bar.append("line")
+        //     .attr("class", "linecap_top")
+        //     .data(confidenceIndicators)
+        //     .attr("x1", function () { return x.bandwidth() / 6 - linecapHalfWidth; })
+        //     .attr("y1", function (d) { return y(d.lci); })
+        //     .attr("x2", function () { return x.bandwidth() / 6 + linecapHalfWidth; })
+        //     .attr("y2", function (d) { return y(d.lci); });
 
         //axes labels
-        var yAxisMidpoint = (height + margin.top)/2 + margin.top;    
-        var paddingLeft = 14;         
-        var yAxisLabel = chart.append("text")
-            .attr("class", "label")
-            .attr("id", "y_axis_label")
-            .text(yAxisTitle)
-            .attr("transform", "translate(" + paddingLeft + ", " + yAxisMidpoint + ")rotate(-90)")                
-            .attr("text-anchor", "middle");         
+        // var yAxisMidpoint = (height + margin.top)/2 + margin.top;    
+        // var paddingLeft = 14;         
+        // var yAxisLabel = chart.append("text")
+        //     .attr("class", "label")
+        //     .attr("id", "y_axis_label")
+        //     .text(yAxisTitle)
+        //     .attr("transform", "translate(" + paddingLeft + ", " + yAxisMidpoint + ")rotate(-90)")                
+        //     .attr("text-anchor", "middle");         
 
         //axes
-        var xAxis = chart.append("g")
-            .attr("class", "axis")             
-            .attr("transform", "translate(" + margin.left + ", " + spaceFromTop + ")")
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-45)");
-        var yAxis = chart.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-            .call(d3.axisLeft(y))
-            .select(".domain").remove(); //remove y-axis line
+        // var xAxis = chart.append("g")
+        //     .attr("class", "axis")             
+        //     .attr("transform", "translate(" + margin.left + ", " + spaceFromTop + ")")
+        //     .call(d3.axisBottom(x))
+        //     .selectAll("text")
+        //     .style("text-anchor", "end")
+        //     .attr("dx", "-.8em")
+        //     .attr("dy", ".15em")
+        //     .attr("transform", "rotate(-45)");
+        // var yAxis = chart.append("g")
+        //     .attr("class", "axis")
+        //     .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+        //     .call(d3.axisLeft(y))
+        //     .select(".domain").remove(); //remove y-axis line
 
         //legend 
-        var legend = chart.append("g") //create & position legend area
-            .attr("class", "legend")
-            .attr("transform", "translate(" + halfTotalWidth + ", " + chartBottomBufferLegend + ")");
-        var legendEntry =  legend.selectAll("g") //groupings do not exist yet
-            .data(legendCategoryNames) //count data
-            .enter() //run methods once per data count
-            .append("g") //produces new groupings
-            .attr("height", legendColorKeyHeight); 
-        var colorKey = legendEntry.append("rect")
-            .attr("width", legendColorKeyWidth)
-            .attr("height", legendColorKeyHeight)
-            .attr("transform", function (d, i) {
-                let legendItemYPosition = legendItemHeight*i;
-                return "translate(0, " + legendItemYPosition + ")";
-            })
-            .style("fill", function (d, i) {
-                return barColors[i];
-            });
-        var label = legendEntry.append("text")
-            .text(function (d) { return d; }) 
-            //.attr("height", legendColorKeyHeight)
-            .attr("transform", function (d, i) {
-                let legendItemYPosition = legendItemHeight*i;
-                let paddingLeft = legendColorKeyWidth*2; 
-                return "translate(" + paddingLeft + ", " + legendItemYPosition + ")";
-        });
+        // var legend = chart.append("g") //create & position legend area
+        //     .attr("class", "legend")
+        //     .attr("transform", "translate(" + halfTotalWidth + ", " + chartBottomBufferLegend + ")");
+        // var legendEntry =  legend.selectAll("g") //groupings do not exist yet
+        //     .data(legendCategoryNames) //count data
+        //     .enter() //run methods once per data count
+        //     .append("g") //produces new groupings
+        //     .attr("height", legendColorKeyHeight); 
+        // var colorKey = legendEntry.append("rect")
+        //     .attr("width", legendColorKeyWidth)
+        //     .attr("height", legendColorKeyHeight)
+        //     .attr("transform", function (d, i) {
+        //         let legendItemYPosition = legendItemHeight*i;
+        //         return "translate(0, " + legendItemYPosition + ")";
+        //     })
+        //     .style("fill", function (d, i) {
+        //         return barColors[i];
+        //     });
+        // var label = legendEntry.append("text")
+        //     .text(function (d) { return d; }) 
+        //     //.attr("height", legendColorKeyHeight)
+        //     .attr("transform", function (d, i) {
+        //         let legendItemYPosition = legendItemHeight*i;
+        //         let paddingLeft = legendColorKeyWidth*2; 
+        //         return "translate(" + paddingLeft + ", " + legendItemYPosition + ")";
+        // });
     }
 } 
