2103d893f055430f441bf77000903fc8e14ab4e3
Alex Windle <alexwindle85@gmail.com>
2018-02-02 08:45:36 -0500

    load new configs for testing

--- data_init.js
+++ data_init.js
@@ -5,10 +5,28 @@ let data_1 = new Promise(function (resolve, reject) {
 	})
 })
 
+let data_2 = new Promise(function (resolve, reject) {
+	d3.json("./resp_2.json", function (data) {
+		resolve(data)
+	})
+})
+
 let data_3 = new Promise(function (resolve, reject ){
     d3.json("./resp_3.json", function (data) {
         resolve(data)
     })
+})
+
+let data_4 = new Promise(function (resolve, reject) {
+	d3.json("./resp_4.json", function (data) {
+		resolve(data)
+	})
+})
+
+let data_5 = new Promise(function (resolve, reject) {
+	d3.json("./resp_5.json", function (data) {
+		resolve(data)
+	})
 })
 
 //load lookup
@@ -18,7 +36,7 @@ var lookup = new Promise(function (resolve, reject) {
 	})
 })
 
-//configuration object, resp_1.json
+//configuration object - resp_1.json
 // var chartConfigObject = {  
 //     "xAxisColumn":"rs", 
 //     "xAxisType":"Response", 
@@ -44,7 +62,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "confidenceIntervalLabel":"95% CI" 
 // }
 
-//configuration object, resp_2.json
+//configuration object - resp_2.json
 // var chartConfigObject = {  
 //     "xAxisColumn":"yr",
 //     "xAxisType":"Year",
@@ -70,11 +88,11 @@ var lookup = new Promise(function (resolve, reject) {
 //     "confidenceIntervalLabel":"95% CI"
 //  }
 
-//configuration object, resp_3.json
+//configuration object - resp_3.json
 var chartConfigObject = {  
-    "xAxisColumn":"s1",
+    "xAxisColumn":"s1", //3 age ranges
     "xAxisType":"Stratification",
-    "legendColumn":"s2",
+    "legendColumn":"s2", // DISABL, NODIS
     "legendType":"Stratification",
     "legendTitleStr":"Disability Status",
     "displayTrendChart":false,
@@ -96,10 +114,65 @@ var chartConfigObject = {
     "confidenceIntervalLabel":"95% CI"
  }
 
-Promise.all([data_1, data_3, lookup]).then(function (data) {
-    //for testing purposes, only turn on 1 data variable at a time
+//configuration object - resp_4.json
+// var chartConfigObject = {  
+//     "xAxisColumn":"rs",
+//     "xAxisType":"Response",
+//     "legendColumn":"s2",
+//     "legendType":"Stratification",
+//     "legendTitleStr":"Disability Status",
+//     "displayTrendChart":false,
+//     "colorsArrStr":[  
+//        "#377eb8",
+//        "#e41a1c",
+//        "#4daf4a",
+//        "#984ea3",
+//        "#ff7f00",
+//        "#a65628",
+//        "#f781bf",
+//        "#CAB2D6",
+//        "#FF7F00",
+//        "#FDBF6F",
+//        "#FFFF99"
+//     ],
+//     "yAxisTitle":"Age-adjusted Prevalence (%)",
+//     "decimalPlaces":1,
+//     "confidenceIntervalLabel":"95% CI"
+//  }
+
+//configuration object - resp_5.json
+// var chartConfigObject = {  
+//     "xAxisColumn":"yr",
+//     "xAxisType":"Year",
+//     "legendColumn":"s1",
+//     "legendType":"Stratification",
+//     "legendTitleStr":"Age Group",
+//     "displayTrendChart":false,
+//     "colorsArrStr":[  
+//        "#377eb8",
+//        "#e41a1c",
+//        "#4daf4a",
+//        "#984ea3",
+//        "#ff7f00",
+//        "#a65628",
+//        "#f781bf",
+//        "#CAB2D6",
+//        "#FF7F00",
+//        "#FDBF6F",
+//        "#FFFF99"
+//     ],
+//     "yAxisTitle":"Age-adjusted Prevalence (%)",
+//     "decimalPlaces":1,
+//     "confidenceIntervalLabel":"95% CI"
+//  }
+
+Promise.all([data_1, data_2, data_3, data_4, data_5, lookup]).then(function (data) {
+    // *only turn on 1 jsonData at a time*
     // var jsonData = data[0]; 
-    var jsonData = data[1]; 
-    var lookup = data[2]; 
+    // var jsonData = data[1]; 
+    var jsonData = data[2]; 
+    // var jsonData = data[3]; 
+    // var jsonData = data[4]; 
+    var lookup = data[5]; 
     makeChart(chartConfigObject, jsonData, lookup); 
 });
