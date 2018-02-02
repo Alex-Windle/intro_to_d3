2ef96af6fb0bed48b572730045bfe270b69719cc
Alex Windle <alexwindle85@gmail.com>
2018-02-02 07:26:43 -0500

    new chart config w/  dataset resp_3

--- data_init.js
+++ data_init.js
@@ -12,31 +12,83 @@ var lookup = new Promise(function(resolve, reject){
 	})
 })
 
-//configuration object
+//configuration object, resp_1.json
+// var chartConfigObject = {  
+//     "xAxisColumn":"rs", 
+//     "xAxisType":"Response", 
+//     "legendColumn":"s1", 
+//     "legendType":"Stratification", 
+//     "legendTitleStr":"", 
+//     "displayTrendChart":false, 
+//     "colorsArrStr":[  
+//         "#377eb8",
+//         "#e41a1c",
+//         "#4daf4a",
+//         "#984ea3",
+//         "#ff7f00",
+//         "#a65628",
+//         "#f781bf",
+//         "#CAB2D6",
+//         "#FF7F00",
+//         "#FDBF6F",
+//         "#FFFF99"
+//     ],
+//     "yAxisTitle":"Age-adjusted Prevalence (%)", 
+//     "decimalPlaces":1,
+//     "confidenceIntervalLabel":"95% CI" 
+// }
+
+//configuration object, resp_2.json
+// var chartConfigObject = {  
+//     "xAxisColumn":"yr",
+//     "xAxisType":"Year",
+//     "legendColumn":"s1",
+//     "legendType":"Stratification",
+//     "legendTitleStr":"Age Group",
+//     "displayTrendChart":true,
+//     "colorsArrStr":[  
+//        "#377eb8",
+//        "#e41a1c",
+//        "#4daf4a",
+//        "#984ea3",
+//        "#ff7f00",
+//        "#a65628",
+//        "#f781bf",
+//        "#CAB2D6",
+//        "#FF7F00",
+//        "#FDBF6F",
+//        "#FFFF99"
+//     ],
+//     "yAxisTitle":"Age-adjusted Prevalence (%)",
+//     "decimalPlaces":1,
+//     "confidenceIntervalLabel":"95% CI"
+//  }
+
+//configuration object, resp_3.json
 var chartConfigObject = {  
-    "xAxisColumn":"rs", 
-    "xAxisType":"Response", 
-    "legendColumn":"s1", 
-    "legendType":"Stratification", 
-    "legendTitleStr":"", 
-    "displayTrendChart":false, 
+    "xAxisColumn":"s1",
+    "xAxisType":"Stratification",
+    "legendColumn":"s2",
+    "legendType":"Stratification",
+    "legendTitleStr":"Disability Status",
+    "displayTrendChart":false,
     "colorsArrStr":[  
-        "#377eb8",
-        "#e41a1c",
-        "#4daf4a",
-        "#984ea3",
-        "#ff7f00",
-        "#a65628",
-        "#f781bf",
-        "#CAB2D6",
-        "#FF7F00",
-        "#FDBF6F",
-        "#FFFF99"
+       "#377eb8",
+       "#e41a1c",
+       "#4daf4a",
+       "#984ea3",
+       "#ff7f00",
+       "#a65628",
+       "#f781bf",
+       "#CAB2D6",
+       "#FF7F00",
+       "#FDBF6F",
+       "#FFFF99"
     ],
-    "yAxisTitle":"Age-adjusted Prevalence (%)", 
+    "yAxisTitle":"Age-adjusted Prevalence (%)",
     "decimalPlaces":1,
-    "confidenceIntervalLabel":"95% CI" 
-}
+    "confidenceIntervalLabel":"95% CI"
+ }
 
 Promise.all([data_1, lookup]).then(function (data) {
     var jsonData = data[0]; 
