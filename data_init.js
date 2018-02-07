d436fe5f308eabacc57d8d9213c30ccf9925d03d
Alex Windle <alexwindle85@gmail.com>
2018-02-07 15:20:52 -0500

    fix syntax

--- data_init.js
+++ data_init.js
@@ -184,7 +184,7 @@ var chartConfigObject = {
     "legendType":"Stratification",
     "legendTitleStr":"Race/Ethnicity",
     "displayTrendChart":false,
-    "chartDivId", "test123",
+    "chartDivId": "test123",
     "colorsArrStr":[  
        "#377eb8",
        "#e41a1c",
