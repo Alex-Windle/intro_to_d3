9186a1888361fe1990687c484f8b8d8d6857ae00
Alex Windle <alexwindle85@gmail.com>
2018-02-12 12:38:43 -0500

    match unique id to tfs

--- data_init.js
+++ data_init.js
@@ -79,7 +79,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "yAxisTitle":"Age-adjusted Prevalence (%)", 
 //     "decimalPlaces":1,
 //     "confidenceIntervalLabel":"95% CI",
-//     "chartDivId": "test123"
+//     "chartDivId": "chartDiv"
 // }
 
 // TEST FOR GROUPED BAR CHART (3 BARS RENDER PER EACH OF 3 RESPONSE GROUPINGS)
@@ -108,7 +108,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "yAxisTitle":"Age-adjusted Prevalence (%)",
 //     "decimalPlaces":1,
 //     "confidenceIntervalLabel":"95% CI",
-//     "chartDivId": "test123",
+//     "chartDivId": "chartDiv",
 //     "chartDesc": "This is a test chart description.",
 //     "chartTitle": "This is a test chart title",
 //     "dataValueSuffix": "%"
@@ -139,7 +139,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "yAxisTitle":"Age-adjusted Prevalence (%)",
 //     "decimalPlaces":1,
 //     "confidenceIntervalLabel":"95% CI",
-//     "chartDivId": "test123"
+//     "chartDivId": "chartDiv"
 //  }
 
 // TEST FOR GROUPED BAR CHART (2 BARS RENDER FOR 1 RESPONSE GROUPING)
@@ -167,7 +167,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "yAxisTitle":"Age-adjusted Prevalence (%)",
 //     "decimalPlaces":1,
 //     "confidenceIntervalLabel":"95% CI",
-//     "chartDivId": "test123"
+//     "chartDivId": "chartDiv"
 //  }
 
 // TEST FOR GROUPED BAR CHART (3 BARS RENDER PER EACH OF 2 RESPONSE GROUPINGS)
@@ -195,7 +195,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "yAxisTitle":"Age-adjusted Prevalence (%)",
 //     "decimalPlaces":1,
 //     "confidenceIntervalLabel":"95% CI",
-//     "chartDivId": "test123"
+//     "chartDivId": "chartDiv"
 //  }
 
 // TEST FOR MISSING OR NULL DATA VALUES
@@ -207,7 +207,7 @@ var lookup = new Promise(function (resolve, reject) {
 //     "legendType":"Stratification",
 //     "legendTitleStr":"Race/Ethnicity",
 //     "displayTrendChart":false,
-//     "chartDivId": "test123",
+//     "chartDivId": "chartDiv",
 //     "colorsArrStr":[  
 //        "#377eb8",
 //        "#e41a1c",
@@ -233,47 +233,14 @@ var lookup = new Promise(function (resolve, reject) {
 // TEST FOR SORTING RESPONSES IN ASCENDING ORDER
 // Replicate on the dev site: California > Health Risks & Behav > Tested for HIV > 2016 > Race/Ethnicity ALL > Disability Status ALL > Response YES
 // configuration object - resp_7.json
-// var chartConfigObject = {  
-//     "xAxisColumn":"s1",
-//     "xAxisType":"Stratification",
-//     "legendColumn":"s2",
-//     "legendType":"Stratification",
-//     "legendTitleStr":"Disability Status",
-//     "displayTrendChart":false,
-//     "chartDivId":"test123",
-//     "colorsArrStr":[  
-//        "#377eb8",
-//        "#e41a1c",
-//        "#4daf4a",
-//        "#984ea3",
-//        "#ff7f00",
-//        "#a65628",
-//        "#f781bf",
-//        "#CAB2D6",
-//        "#FF7F00",
-//        "#FDBF6F",
-//        "#FFFF99"
-//     ],
-//     "yAxisTitle":"Age-adjusted Prevalence (%)",
-//     "decimalPlaces":1,
-//     "sampleSizeLabel":"Weighted No.",
-//     "dataValuePrefix":"",
-//     "dataValueSuffix":"%",
-//     "chartTitle508":"chart 508 title goes here",
-//     "chartDesc508":"chart 508 desc goes here"
-//  }; 
-
-// TEST FOR SORTING LEGEND COLUMN IN ASCENDING ORDER
-// Replicate on the dev site: California > Prev & Screen > Mammogram past 2 yr > All yrs > Race/Ethnicity ALL > Disability Status ANY DIS > Response YES
-// configuration object - resp_8.json
 var chartConfigObject = {  
-    "xAxisColumn":"yr",
-    "xAxisType":"Year",
+    "xAxisColumn":"s1",
+    "xAxisType":"Stratification",
     "legendColumn":"s2",
     "legendType":"Stratification",
-    "legendTitleStr":"Race/Ethnicity",
-    "displayTrendChart":true,
-    "chartDivId":"test123",
+    "legendTitleStr":"Disability Status",
+    "displayTrendChart":false,
+    "chartDivId":"chartDiv",
     "colorsArrStr":[  
        "#377eb8",
        "#e41a1c",
@@ -287,14 +254,47 @@ var chartConfigObject = {
        "#FDBF6F",
        "#FFFF99"
     ],
-    "yAxisTitle":"Crude Prevalence (%)",
+    "yAxisTitle":"Age-adjusted Prevalence (%)",
     "decimalPlaces":1,
     "sampleSizeLabel":"Weighted No.",
     "dataValuePrefix":"",
     "dataValueSuffix":"%",
     "chartTitle508":"chart 508 title goes here",
     "chartDesc508":"chart 508 desc goes here"
- }
+ }; 
+
+// TEST FOR SORTING LEGEND COLUMN IN ASCENDING ORDER
+// Replicate on the dev site: California > Prev & Screen > Mammogram past 2 yr > All yrs > Race/Ethnicity ALL > Disability Status ANY DIS > Response YES
+// configuration object - resp_8.json
+// var chartConfigObject = {  
+//     "xAxisColumn":"yr",
+//     "xAxisType":"Year",
+//     "legendColumn":"s2",
+//     "legendType":"Stratification",
+//     "legendTitleStr":"Race/Ethnicity",
+//     "displayTrendChart":true,
+//     "chartDivId":"chartDiv",
+//     "colorsArrStr":[  
+//        "#377eb8",
+//        "#e41a1c",
+//        "#4daf4a",
+//        "#984ea3",
+//        "#ff7f00",
+//        "#a65628",
+//        "#f781bf",
+//        "#CAB2D6",
+//        "#FF7F00",
+//        "#FDBF6F",
+//        "#FFFF99"
+//     ],
+//     "yAxisTitle":"Crude Prevalence (%)",
+//     "decimalPlaces":1,
+//     "sampleSizeLabel":"Weighted No.",
+//     "dataValuePrefix":"",
+//     "dataValueSuffix":"%",
+//     "chartTitle508":"chart 508 title goes here",
+//     "chartDesc508":"chart 508 desc goes here"
+//  }
 
 Promise.all([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, lookup]).then(function (data) {
     // *only turn on 1 jsonData at a time*
@@ -304,8 +304,8 @@ Promise.all([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, loo
     // var jsonData = data[3]; // TEST FOR GROUPED BAR CHART (2 BARS RENDER FOR 1 RESPONSE GROUPING) 
     // var jsonData = data[4]; // TEST FOR GROUPED BAR CHART (3 BARS RENDER PER EACH OF 2 RESPONSE GROUPINGS)
     // var jsonData = data[5]; // TEST FOR MISSING OR NULL DATA VALUES
-    // var jsonData = data[6]; // TEST FOR SORTING RESPONSES IN ASCENDING ORDER
-    var jsonData = data[7]; // TEST FOR SORTING LEGEND COLUMN IN ASCENDING ORDER
+    var jsonData = data[6]; // TEST FOR SORTING RESPONSES IN ASCENDING ORDER
+    // var jsonData = data[7]; // TEST FOR SORTING LEGEND COLUMN IN ASCENDING ORDER
     var lookup = data[8]; 
     makeChart(chartConfigObject, jsonData, lookup); 
 });
